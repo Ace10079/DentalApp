5ee6f63924e85efcab33d8a8ab01831300113558
Ace10079 <minions10karan@gmail.com>
2024-09-03 06:06:39 +0530

    data testing 3

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
+  const navigate = useNavigate(); // Call useNavigate to get the navigate function
+
+  const handleLogout = () => {
+    // Clear token on logout (you can implement this)
+    // Clear adminName from context (if using context)
+
+    navigate('/login'); // Redirect to login page
+  };
+
   return (
     <div className='flex lg:flex-row flex-col p-4 gap-5'>
       <div className='lg:w-44 lg:h-[450px] bg-white p-3 rounded-md'>
@@ -9,12 +19,12 @@ function Profile() {
       <div className='w-full bg-white h-[450px] rounded-md'>
         <div className="p-10">
           <label htmlFor="" className="font-semibold">
-          Name
+            Name
           </label>
           <input
             type="text"
             className="w-full border px-2 py-1 mb-2 rounded-md bg-slate-200"
-            placeholder="Name"
+            placeholder="Admin_Name"
           />
          
           <label htmlFor="" className="font-semibold">
@@ -23,18 +33,17 @@ function Profile() {
           <input
             type="text"
             className="w-full border px-2 py-1 mb-2 rounded-md bg-slate-200"
-            placeholder="abc@gmail.com"
+            placeholder="Admin_Email"
           />
          
-           <div className="flex justify-center gap-2 lg:mt-5 mt-12">
-                <button
-                  className="border border-black text-black px-7 py-2 ml-2 rounded-full "
-                  onClick={() => setEditModal(false)}
-                >
-                  Logout
-                </button>
-                
-              </div>
+          <div className="flex justify-center gap-2 lg:mt-5 mt-12">
+            <button
+              className="border border-black text-black px-7 py-2 ml-2 rounded-full"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </div>
